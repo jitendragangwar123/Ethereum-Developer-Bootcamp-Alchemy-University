50d55c5b28024983a421a79eb49566f8d1dacbf6
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-20 17:58:00 +0530

    initial commit

--- Week-1/2_Public_Key_Cryptography.js
+++ Week-1/2_Public_Key_Cryptography.js
@@ -115,3 +115,68 @@ describe('Sign Message', () => {
         assert.equal(toHex(recovered), toHex(publicKey));
     });
 });
+
+
+/*
+Recover the Public Key :-
+                  1.When the signature is passed with all of its components (recovery bit included), the public key can be recovered. 
+                  2.This means that blockchain nodes will be able to understand who signed the transaction that was sent to them. 
+*/
+
+//recoverKey.js
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require("./hashMessage");
+
+async function recoverKey(message, signature, recoveryBit) {
+    const messageHash=hashMessage(message);
+    return secp.recoverPublicKey(messageHash,signature,recoveryBit);
+}
+
+module.exports = recoverKey;
+
+//signMessage.js
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require('./hashMessage');
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+async function signMessage(msg) {
+    const messageHash=hashMessage(msg);
+    return secp.sign(messageHash,PRIVATE_KEY,{recovered:true});
+}
+
+module.exports = signMessage;
+
+//hashMessage.js
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    const bytes=utf8ToBytes(message);
+    const hash=keccak256(bytes);
+    return hash;
+    
+}
+
+module.exports = hashMessage;
+
+//test.js
+const signMessage = require('../signMessage');
+const recover = require('../recoverKey');
+const secp = require("ethereum-cryptography/secp256k1");
+const { assert } = require('chai');
+const { toHex } = require("ethereum-cryptography/utils");
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+describe('Recover Key', () => {
+    it('should recover the public key from a signed message', async () => {
+        const [sig, recoveryBit] = await signMessage('hello world');
+
+        const publicKey = secp.getPublicKey(PRIVATE_KEY);
+
+        const recovered = await recover('hello world', sig, recoveryBit);
+
+        assert.equal(toHex(recovered), toHex(publicKey));
+    });
+});
