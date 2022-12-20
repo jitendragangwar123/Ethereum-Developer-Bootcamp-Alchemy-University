a76d5adfa99bd3e1741b498ee5c28e3cb7ff716f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-20 17:44:47 +0530

    initial commit

--- Week-1/2_Public_Key_Cryptography.js
+++ Week-1/2_Public_Key_Cryptography.js
@@ -43,3 +43,75 @@ describe('Hash Message', () => {
         assert.equal(toHex(messageHash), helloWorldHex);
     });
 });
+
+
+/*
+Sign Message :-
+            To sign a message using our private key to prove our intention!
+Steps:-
+1.When signing a message with secp256k1 we can return the signature along with the recovery bit, 
+allowing us to recover the public key from the signature. This will allow a blockchain node to take a signature of a transaction 
+and understand which address authenticated this particular transaction.
+
+2.The sign method takes an optional third parameter called options, which you'll see in the documentation. 
+Use this parameter to return the recovered bit so that the public key can be recovered from this signature.
+
+*/
+
+//signMessage.js
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require('./hashMessage');
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+async function signMessage(msg) {
+    const messageHash=hashMessage(msg);
+    return secp.sign(messageHash,PRIVATE_KEY,{recovered:true});
+}
+
+module.exports = signMessage;
+
+//hashMessage.js
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    const bytes=utf8ToBytes(message);
+    const hash=keccak256(bytes);
+    return hash;
+    
+}
+
+module.exports = hashMessage;
+
+//test.js
+const signMessage = require('../signMessage');
+const hashMessage = require('../hashMessage');
+const { assert } = require('chai');
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+describe('Sign Message', () => {
+    it('should return both a signature and a recovery bit', async () => {
+        const response = await signMessage('hello world');
+
+        const errMessage = "expected signMessage to return both a signature and recovery bit!";
+        assert(response.length, errMessage);
+        assert(response.length === 2, errMessage);
+
+        const [signature, recoveryBit] = response;
+        assert(signature.length, "expected signature to be a Uint8Array");
+        assert(typeof recoveryBit === "number", "expected the recovery bit to be a number");
+    });
+
+    it('should have been signed by the same private key', async () => {
+        const [sig, recoveryBit] = await signMessage('hello world');
+        const messageHash = hashMessage('hello world');
+        const recovered = secp.recoverPublicKey(messageHash, sig, recoveryBit);
+
+        const publicKey = secp.getPublicKey(PRIVATE_KEY);
+        assert.equal(toHex(recovered), toHex(publicKey));
+    });
+});
