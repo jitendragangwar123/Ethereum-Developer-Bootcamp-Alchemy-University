cde66ce2456cb1d153fb6227884b2b8834223c1f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-04-13 16:32:20 +0530

    key examples added

--- Week-1/2_Public_Key_Cryptography.js
+++ Week-1/2_Public_Key_Cryptography.js
@@ -1,4 +1,44 @@
 /*
+Symmetric Key Encryption :- 
+1. It only requires a single key for both encryption and decryption.
+2. The encryption process is very fast.
+3. Security is less as only one key is used for both encryption and decryption purpose.
+4. The length of key used is 128 or 256 bits.
+5. The size of cipher text is the same or smaller than the original plain text.
+6. The Mathematical Representation is as follows-
+      P = D (K, E(P))
+
+      where K –> encryption and decryption key
+      P –> plain text
+      D –> Decryption 
+      E(P) –> Encryption of plain text
+      
+ 7. Examples: 3DES, AES, DES and RC4     
+*/
+
+/*
+Asymmetric Key Encryption :- 
+1. It requires two keys, a public key and a private key, one to encrypt and the other one to decrypt.
+2. The encryption process is slow.
+3. The size of cipher text is the same or larger than the original plain text.
+4. The  length of key used is 2048 or higher
+5. It is more secure as two keys are used here- one for encryption and the other for decryption.
+6. The Mathematical Representation is as follows-
+      P = D(Kd, E (Ke,P))
+      where Ke –> encryption key
+
+      Kd –> decryption key
+      D –> Decryption
+      E(Ke, P) –> Encryption of plain text using encryption key Ke . P –> plain text
+      
+7. Examples: Diffie-Hellman, ECC, El Gamal, ECDSA and RSA     
+*/
+
+/*
+Note :- public key cryptography is considered asymmetric encryption in that only one party has access to the private key.
+*/
+
+/*
 RSA and ECDSA are two popularly used algorithms for public key cryptography.
 
 RSA:- The RSA algorithm is based on the idea that it's very easy to find the product of two prime numbers, 
@@ -188,6 +228,7 @@ Public Key to Address :-
                   2.For Bitcoin it includes a checksum and Base58 encoding.
                   3.Ethereum's address transformation is quite a bit simpler, its address is the last 20 bytes of the hash of the public key.
                   4.The first byte indicates the format of the key, whether it is in the compressed format or not.
+                  5.The publicKey will be a Uint8Array so you can use the slice method to slice off the first byte.
 */
 
 //getAddress.js
