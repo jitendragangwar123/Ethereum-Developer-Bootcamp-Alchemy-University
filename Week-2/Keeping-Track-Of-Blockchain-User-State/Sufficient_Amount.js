31dcc75a8a2db31c7a63e5fcede3bc63f994e237
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-16 14:46:45 +0530

    feat:Sufficient_Amount.js file added

---
+++ Week-2/Keeping-Track-Of-Blockchain-User-State/Sufficient_Amount.js
@@ -0,0 +1,84 @@
+//Transaction.js
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs = inputUTXOs;
+        this.outputUTXOs = outputUTXOs;
+    }
+    execute() {
+        const anySpent = this.inputUTXOs.some((x) => x.spent);
+        if (anySpent) {
+            throw new Error("Cannot include a spent UTXO");
+        }
+
+        const inputAmount = this.inputUTXOs.reduce((p, c) => {
+            return p + c.amount;
+        }, 0);
+        const outputAmount = this.outputUTXOs.reduce((p, c) => {
+            return p + c.amount;
+        }, 0);
+        if (inputAmount < outputAmount) {
+            throw new Error("Not enough here");
+        }
+    }
+}
+module.exports = Transaction;
+
+//TXO.js
+class TXO {
+    constructor(owner, amount) {
+        this.owner=owner;
+        this.amount=amount;
+        this.spent=false;
+    }
+    spend() {
+        this.spent=true;
+    }
+}
+module.exports = TXO;
+
+//test.js
+const { assert } = require('chai');
+const Transaction = require('../Transaction');
+const TXO = require('../TXO');
+
+describe('Transaction', function () {
+    const fromAddress = "1DBS97W3jWw6FnAqdduK1NW6kFo3Aid1N6";
+    const toAddress = "12ruWjb4naCME5QhjrQSJuS5disgME22fe";
+    
+    describe('with sufficient UTXOs', () => {
+        const txo1 = new TXO(fromAddress, 5);
+        const txo2 = new TXO(fromAddress, 5);
+        const outputTXO1 = new TXO(toAddress, 10);
+        const tx = new Transaction([txo1, txo2], [outputTXO1]);
+
+        it('should execute without error', () => {
+            try {
+                tx.execute();
+            }
+            catch (ex) {
+                assert.fail(ex.message);
+                console.log(ex);
+            }
+        });
+    });
+
+    describe('with insufficient UTXOs', () => {
+        const txo1 = new TXO(fromAddress, 3);
+        const txo2 = new TXO(fromAddress, 3);
+        const txo3 = new TXO(fromAddress, 3);
+        const outputTXO1 = new TXO(toAddress, 10);
+
+        const tx = new Transaction([txo1, txo2, txo3], [outputTXO1]);
+
+        it('should throw an error on execute', () => {
+            let ex;
+            try {
+                tx.execute();
+            }
+            catch (_ex) {
+                ex = _ex;
+            }
+            assert(ex, "Did not throw an exception for a lack of UTXO funds!");
+        });
+    });
+});
