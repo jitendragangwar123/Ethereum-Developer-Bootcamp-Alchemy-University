0e47c167732c11e8063ec1fc27800b2f71cf4328
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-16 15:41:31 +0530

    feat:Tree.js file added

---
+++ Week-2/Tree-Data-Structure/Tree.js
@@ -0,0 +1,29 @@
+/*
+Binary Tree:
+  Each parent has at most two children
+*/
+
+/*
+//LinedList
+class LinkedListNode {
+	constructor(data) {
+	    this.data = data;
+	    this.next = null;
+	}
+}
+//Tree
+class TreeNode {
+	constructor(data) {
+	    this.data = data;
+	    this.children = [];
+	}
+}
+*/
+
+/*
+Binary Search Tree:
+  1. It is a binary tree
+  2. The left subtree of a node contains only nodes with keys lesser than the node's key
+  3. The right subtree of a node contains only nodes with keys greater than the node's key
+  4. Each node’s left and right subtrees must also be a binary search tree.
+*/
