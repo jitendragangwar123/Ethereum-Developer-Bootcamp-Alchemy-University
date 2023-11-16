aad6c648bb91ec22213bb9dc181e43331c5ceabc
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-16 14:32:04 +0530

    fix: Transaction.js updated

--- Week-2/Keeping-Track-Of-Blockchain-User-State/Spent_TXOs.js
+++ Week-2/Keeping-Track-Of-Blockchain-User-State/Spent_TXOs.js
@@ -8,26 +8,9 @@ class Transaction {
         const anySpent = this.inputUTXOs.some((x) => x.spent);
         if (anySpent) {
             throw new Error("Cannot include a spent UTXO");
-        }
-
-        const inputAmount = this.inputUTXOs.reduce((p, c) => {
-            return p + c.amount;
-        }, 0);
-        const outputAmount = this.outputUTXOs.reduce((p, c) => {
-            return p + c.amount;
-        }, 0);
-        if (inputAmount < outputAmount) {
-            throw new Error("Not enough here");
-        }
-
-        this.inputUTXOs.forEach((utxos)=>{
-            utxos.spend();
-        });
-
-        this.fee=inputAmount-outputAmount;
+        }  
     }
 }
-
 module.exports = Transaction;
 
 //TXO.js
