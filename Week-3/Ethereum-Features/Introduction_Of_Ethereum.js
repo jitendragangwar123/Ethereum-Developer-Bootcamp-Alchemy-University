fa6c7913b1c93e4d2f5004e0468b1b519c087cda
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-17 17:42:07 +0530

    feat:Introduction_Of_Ethereum.js file added

---
+++ Week-3/Ethereum-Features/Introduction_Of_Ethereum.js
@@ -0,0 +1,29 @@
+/*
+Ethereum:
+     1. Ethereum is a deterministic but practically unbounded state machine, consisting of a globally accessible singleton 
+     state and a virtual machine that applies changes to that state.
+     2. Ethereum is an open source, globally decentralized computing infrastructure that executes programs called smart contracts.
+     3. Ethereum = A Computer
+*/
+
+/*
+                         |       ETHEREUM	                    |        BITCOIN
+Consensus Mechanism	     |     Proof of Stake	                |      Proof of Work
+Accounting System	       |    Account Model                   |      	UTXO Model
+Public Key Cryptography	 |   secp256k1 elliptic curve	        | secp256k1 elliptic curve
+Stale/Orphan Blocks	     |  Rewarded (Ommer Blocks)	          |      Not Rewarded
+Block Time	             |  Approx every 12 seconds	          | Approx every 10 minutes
+Network Difficulty	     |  Adjusted every block	            | Every 2016 blocks
+Language Support	       | Turing Complete smart contracts,   | non-Turing Complete scripts
+                         |  custom VMVM operations cost gas.	|    
+*/
+
+/*
+The Ethereum Virtual Machine:
+    A Virtual Machine is a program that emulates a particular environment for code to run in.
+*/
+
+/*
+Gas:
+  Gas is a measurement of the cost to each operation that relates to the computational cost that the operation incurs on the network.
+*/
