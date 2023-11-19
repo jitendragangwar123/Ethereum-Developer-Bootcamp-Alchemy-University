28a12889b13ff05c86a19fa25c88f5856de98920
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-19 16:51:07 +0530

    feat:Introduction.js file added

---
+++ Week-3/Ethereum-Transactions/Introduction.js
@@ -0,0 +1,8 @@
+/*
+Intro to Ethereum Transactions:
+    1. An Ethereum transaction refers to an action initiated by an EOA (externally-owned account), 
+       in other words an account managed by a human, not a contract.
+    2. Ethereum = A Transaction-Based State Machine.
+    3. Transactions are collected into blocks. 
+    4. A block is a package of data.
+*/
