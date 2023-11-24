079fcd55d106316bbec145a6b7ec1e57e2a77cf2
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-24 22:54:03 +0530

    feat:ContractCompilation.sol file added

--- Week-4/Smart-Contract-Communication/ContractCompilation.sol
+++ Week-4/Smart-Contract-Communication/ContractCompilation.sol
@@ -5,3 +5,58 @@ Contract Compilation Produces Two Artifacts:
   2.The contract's bytecode
         We deploy the bytecode directly to the blockchain, in which case it is stored directly in the contract account's state trie
 */
+
+/*
+require('dotenv').config();
+const ethers = require('ethers');
+
+const contractABI = [
+  {
+    inputs: [],
+    name: 'count',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'dec',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'get',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'inc',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+const provider = new ethers.providers.AlchemyProvider(
+  'goerli',
+  process.env.TESTNET_ALCHEMY_KEY
+);
+
+const wallet = new ethers.Wallet(process.env.TESTNET_PRIVATE_KEY, provider);
+
+async function main() {
+  const counterContract = new ethers.Contract(
+    '0x5F91eCd82b662D645b15Fd7D2e20E5e5701CCB7A',
+    contractABI,
+    wallet
+  );
+
+  await counterContract.inc();
+}
+
+main();
+*/
